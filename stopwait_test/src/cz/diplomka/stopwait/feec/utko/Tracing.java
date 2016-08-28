8979d0a37d9ee808cd0718b530978653d978de9c
adrian <adrian@adrian-X550CC>
2016-08-28 22:18:14 +0200

    prvy commit testovacieho kodu

---
+++ stopwait_test/src/cz/diplomka/stopwait/feec/utko/Tracing.java
@@ -0,0 +1,22 @@
+package cz.diplomka.stopwait.feec.utko;
+
+public class Tracing {
+private static boolean isOn = true;	//if Tracing.isOn=true application will print out activities 
+	
+	public static boolean isOn() {
+		return isOn;
+	}
+	
+	public static void enable() {
+		isOn = true;
+	}
+	
+	public static void disable() {
+		isOn = false;
+	}
+	
+	public static void print(String string) {
+		System.out.println(string);
+	}
+
+}
