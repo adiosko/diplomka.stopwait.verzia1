0b8d6709169052d235b088e909880524ca8b4949
adrian <adrian@adrian-X550CC>
2016-09-04 15:19:55 +0200

    Trace edited

--- stopwait_test/src/cz/diplomka/stopwait/feec/utko/Tracing.java
+++ stopwait_test/src/cz/diplomka/stopwait/feec/utko/Tracing.java
@@ -1,20 +1,24 @@
 package cz.diplomka.stopwait.feec.utko;
 
 public class Tracing {
-private static boolean isOn = true;	//if Tracing.isOn=true application will print out activities 
+private static boolean isOn = true;	//isOn je zapnutý tracing, inak nie 
 	
+    //metóda nastavená na trasovanie prenosu
 	public static boolean isOn() {
 		return isOn;
 	}
 	
+	//metóda zapnutie
 	public static void enable() {
 		isOn = true;
 	}
 	
+	//metóda vypnutie
 	public static void disable() {
 		isOn = false;
 	}
 	
+	//metóda vypíše trace
 	public static void print(String string) {
 		System.out.println(string);
 	}
