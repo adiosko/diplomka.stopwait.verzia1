52883cebfca0359f2bf9741eb4c906231fe5501e
adrian <adrian@adrian-X550CC>
2016-09-04 15:18:02 +0200

    Timestats edited

--- stopwait_test/src/cz/diplomka/stopwait/feec/utko/TimeStats.java
+++ stopwait_test/src/cz/diplomka/stopwait/feec/utko/TimeStats.java
@@ -2,38 +2,42 @@ package cz.diplomka.stopwait.feec.utko;
 
 public class TimeStats {
 	private long elapsedTime;		//total elapsed time
-
+	
+	//konštruktor nastaví čas na hodnotu 0
 	public TimeStats() {
 		elapsedTime = 0;
 	}
 	
+	//getter
 	public long getElapsedTime() {
 		return elapsedTime;
 	}
 	
+	//metóda an pridanie času
 	public void add(long time) {
 		elapsedTime += time;
 	}
 	
-	//simulated throughput
+	//metóda na simulovanie priepustnosti siete
 	public double throughputSim(int framesToSendNo, long infoTransTime) {
 		return ((double)(framesToSendNo * infoTransTime)) / elapsedTime;
 	}
 	
-	//theoretical throughput
+	//metóda na výpočet teoretickej priepustnosti na základe (informatívny čas transmitu támca)
+	// /(timeOut + infoTransTime) * frameErrProb / (1.0 - frameErrProb) + infoTransTime + 2 * propDelay + procTime + ackTransTime;
 	public double throughputTheor(long infoTransTime, long ackTransTime, long timeOut, double frameErrProb, long propDelay, long procTime) {
 		double a = (timeOut + infoTransTime) * frameErrProb / (1.0 - frameErrProb);
 		double b = infoTransTime + 2 * propDelay + procTime + ackTransTime;
 		return (double)infoTransTime / (a + b);
 	}
 	
-	//compares simulated and theoretical throughput in %
+	//metóda porovnanie teoretickej a praktickej priepustnosti vyjadrená v %
 	public void throughputEval(double throughputSim, double throughputTheor) {
 		System.out.format("%n\tSimulated throughput:\t %05.2f%%%n", throughputSim * 100);
 		System.out.format("\tTheoretical throughput:\t %05.2f%%%n", throughputTheor * 100);
 	}
 	
-	//calculate optimal info frame length, given bit error probability
+	//výpočet optimálnej dĺžky rámca, na základe prijatej pravdepodbnosti erroru
 	public int optInfoLength(Input in) {
 		return 8192;
 	}
