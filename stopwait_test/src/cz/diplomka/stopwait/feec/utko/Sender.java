f1fed5c33774013658b9ff8617a66947700d2141
adrian <adrian@adrian-X550CC>
2016-09-04 15:13:40 +0200

    Sender edited with description

--- stopwait_test/src/cz/diplomka/stopwait/feec/utko/Sender.java
+++ stopwait_test/src/cz/diplomka/stopwait/feec/utko/Sender.java
@@ -1,18 +1,20 @@
 package cz.diplomka.stopwait.feec.utko;
 
 public class Sender {
-	private static final byte CONST = 0;	//constant lenthDistrib
-//	private static final byte EXP = 1;		//exponential lengthDistrib
+	private static final byte CONST = 0;	//konštantná dĺžka ditribúcie rámca
+//	private static final byte EXP = 1;		//exponenciálna dĺžka distribúcie rámca
 	
-	private byte 		currentFrameNo;		//current info frame number, mod2 (0 or 1)
-	private boolean		timedOut;			//if sender timed-out
-	private long 		timeOutPeriod;		//time out period (microseconds)
-	private int 		infoFrameLength;	//d+i, info frame length (bits)
-	private int 		ackFrameLength;		//d, ack frame length (bits)
-	private byte 		lengthDistrib;		//information data length distribution (CONST or EXP)
-	private Frame 		frameCopy;			//current frame, a copy saved for retransmission
-	private FrameStats 	stats;				//sender's frame statistics
-	
+	private byte 		currentFrameNo;		//súčasné číslo rámca, mod2 (0 or 1)
+	private boolean		timedOut;			//v prípad eže bdue mať sender timeout
+	private long 		timeOutPeriod;		//timeout perióda (microseconds)
+	private int 		infoFrameLength;	//d+i, INFO rámec dĺžka (bits)
+	private int 		ackFrameLength;		//d, ack rámec dĺžka (bits)
+	private byte 		lengthDistrib;		//dĺžka distribúcie (informácia) (CONST or EXP)
+	private Frame 		frameCopy;			//súčasný rámec, kópia pre retrnasmit
+	private FrameStats 	stats;				//štatistika sender rámca
+	 
+	//konštruktor nastaví sa timeout(True, False, prioda timeoutu, info alebo ackdlžka rámca a 
+	//kopia ramca na tue false v prípade že rámec je poškodený alebo zlyhal transmit)
 	public Sender(int totalFrameNo, long timeOut, int aLength, int iLength, byte lDistrib) {
 		currentFrameNo = 1;	//to make sure first frame sent will have number 0
 		timedOut = false;
@@ -23,7 +25,7 @@ public class Sender {
 		frameCopy = null;
 		stats = new FrameStats(totalFrameNo);
 	}
-	
+	//metóda ktorá vráti hodnotu timeoutu
 	public boolean isTimedOut() {
 		return timedOut;
 	}
@@ -52,11 +54,15 @@ public class Sender {
 		return stats;
 	}
 	
+	//metóda ktorá vypočíta dĺžku transmitu rámcu podľa vzťahu dĺžka rámca * 1000000 / (b/s)
 	public long frameTransTime(int length, long dataRate) {
 		return (long)length*1000000 / dataRate;
-		//*1000000 because dataRate is in bits per second and result should be in microseconds
+		//*1000000 because dataRate is in bits per second and result should be in microsecond
 		//mustn't divide dataRate by 1000000 because if result is <1 it will be truncated to 0 when converted to long
 	}
+	
+	//pri zlyhaní tansmitu, metóda vytvorí nový rámec podľa typu CONST alebo Exponencial, 
+	//vytvorí aktuálnu dlžku rámca a inkrementuje číslo rámca 
 	
 	public Frame takeNewFrame() {		//creates new frame for transmission
 		int frameLength;
@@ -89,13 +95,14 @@ public class Sender {
 			currentFrameNo = 0;
 	}
 	
-	//sends (returns) frame with next number
+	//metóda odošle rámec , nenastaví timeout a vráti rámec
 	public Frame send(Frame f) {
 		stats.frameSent();
 		timedOut = false;
 		return f;
 	}
 	
+	//resendne rámec, nastaví mu nové číslo, a vráti kopiu rámca 
 	public Frame resend() {
 		stats.frameSent();
 		timedOut = false;
@@ -107,6 +114,7 @@ public class Sender {
 	 * if ack is ok send next
 	 * if ack is bad do immeadiate resend (don't wait for timeout)
 	 */////////////////////////////////check whether to immediately resend or wait for timeout!!!!!!!!!!!!!!!!
+	//metóda príjme rámec, ak rámec obsahuje error ho nepríjme (false) inak ho príjme(teda nie je porušený)
 	public boolean receive(Frame f) {
 		stats.frameReceived(f.hasError());
 		if(f.hasError() || (f.getNumber() != frameCopy.getNumber()))
